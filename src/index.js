4289a2ac804ef40bf866cf03171989661d839866
Bair <liberty2217@yandex.ru>
2021-03-10 20:04:31 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 module.exports = function reverse (n) {
-    return +(('' + Math.abs(n)).split('').reverse().join(''));
-};
+  
+    let stringN = n.toString().split('').reverse().join('')
 
+    if (stringN.includes('-')) {
+      return stringN.slice(0, -1);
+      //если в строке есть знак "-" мы удаляем последний символ (этот минус)
+    } else {
+       return stringN;
+    }
+
+};
